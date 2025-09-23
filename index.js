f6571f62e2b8d16fc25ff5ee8ce707dd96aad20c
jankoneli <jankoneli@proton.me>
2025-09-23 20:59:00 +0700

    TMC9A V3

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 app.use("/src", express.static("public"))
 
+
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname+"/index.html");
 })
@@ -10,8 +12,15 @@ app.get("/students", (req, res) => {
     res.sendFile(__dirname+"/students.html");
 })
 
-app.get("/agenda", (req, res) => {
-    res.sendFile(__dirname+"/agenda.html");
+app.get("/weekly", (req, res) => {
+    res.sendFile(__dirname+"/weekly.html");
 })
 
+app.get("/u/:id", (req, res) => {
+    res.sendFile(__dirname+"/user.html")
+})
+
+app.use(function(req,res){
+    res.status(404).sendFile(__dirname+"/underconstruction.html");
+});
 app.listen(3030)
