2a2797af6a2734d856217232e4a86622a84a11c6
jankoneli <jankoneli@proton.me>
2025-10-26 04:57:34 +0700

    Added studsos hero image

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 app.use("/src", express.static("public"))
 
-
+var imgstud = {
+    "5":"5.jpg"
+}
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname+"/index.html");
@@ -12,12 +14,24 @@ app.get("/students", (req, res) => {
     res.sendFile(__dirname+"/students.html");
 })
 
+app.get("/agenda", (req, res) => {
+    res.sendFile(__dirname+"/agenda.html");
+})
+
 app.get("/weekly", (req, res) => {
     res.sendFile(__dirname+"/weekly.html");
 })
 
 app.get("/u/:id", (req, res) => {
     res.sendFile(__dirname+"/user.html")
+})
+
+app.get("/img/:id", (req, res) => {
+    if(Object.keys(imgstud).includes(req.params.id.toString())){
+        res.sendFile(__dirname+"/img/"+imgstud[parseInt(req.params.id)]);
+    }else{
+        res.sendFile(__dirname+"/img/placeholder.png");
+    }
 })
 
 app.use(function(req,res){
