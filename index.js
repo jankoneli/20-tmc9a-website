7f6d0b20d88c513f415c87b6dedf86a896c2e4bb
jankoneli <jankoneli@proton.me>
2025-12-08 17:55:06 +0700

    Responsive design for archive page & alt text

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { createServer } = require('node:http');
 const { Server } = require("socket.io");
 app.use("/src", express.static("public"))
 
-
+const fs = require("fs");
 const server = createServer(app);
 const io = new Server(server);
 
@@ -14,6 +14,17 @@ var agendalist = "";
 var imgstud = {
     "5":"5.jpg"
 }
+
+var alttext = {
+
+}
+
+var alttextlines = fs.readFileSync(__dirname+"/alttext")
+alttextlines.toString().split("\n").forEach(line => {
+    alttext[line.split(",")[0]] = line.split(",")[1].replace("\r", "")
+})
+
+console.log(alttext)
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname+"/index.html");
@@ -28,7 +39,7 @@ imagelist = {
     "studsos":[]
 }
 
-const fs = require("fs");
+
 var primary = fs.readdirSync(__dirname+"/public/archive/sd");
 
 primary.forEach(file => {
@@ -48,7 +59,7 @@ io.on('connection', (socket) => {
         socket.emit('agendadata', [agendatitle, agendalist]);
     })
     socket.on('requestimage', (img) => {
-        socket.emit('imagelist', imagelist[img])
+        socket.emit('imagelist', imagelist[img], alttext)
     })
     socket.on('updateagenda', (pin, datadata, datedate) => {
         console.log(parseInt(pin))
